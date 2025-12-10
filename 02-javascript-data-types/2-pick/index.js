bc8460915ab4ea35438a4c4b5500f4b94567b032
grigorii <gsold2@gmail.com>
2025-12-10 23:24:49 +0300

    02_2: create function pick

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,9 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+    return Object.fromEntries(
+        Object.entries(obj)
+            .filter(e => fields.includes(e[0]))
+            .map(([key, value]) => [key, value])
+    );
 };
