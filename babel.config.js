d850e3faad0ff232e1392e8aa6e152c568561c1e
grigorii <gsold2@gmail.com>
2025-12-22 22:07:10 +0300

    minor update

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      targets: {
+        browsers: '> 3%'
+        // browsers: '> 3%, ie 11' // ie 11 transpiles classes
+      }
+    }]
+  ],
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-private-methods'
+  ]
+};
