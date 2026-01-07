d6c0a0a94f963c50eb70e521c63a112d16783b47
grigorii <gsold2@gmail.com>
2026-01-07 14:02:36 +0300

    solutions for the lesson 5

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,66 @@
 export default class NotificationMessage {
+    static #activeNotification = null;
+    #message;
+    #duration;
+    #type;
+    #element;
 
+    constructor(message = 'message',  { duration = 2000, type = 'success' } = {}) {
+        this.#message = message;
+        this.#duration = duration;
+        this.#type = type;
+        this.#createElemente();
+    }
+
+    #createElemente() {
+        const tmp = document.createElement('div');
+        tmp.innerHTML = this.#template();
+        this.#element = tmp.firstElementChild;
+    }
+
+    #template() {
+        return `
+        <div class="notification ${this.#type}" style="--value:${Math.floor(this.#duration / 1000)}s">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.#type}</div>
+                <div class="notification-body">${this.#message}</div>
+            </div>
+        </div>`;
+    }
+
+    show(element = document.body) {
+        if (NotificationMessage.#activeNotification) {
+            NotificationMessage.#activeNotification.remove();
+        }
+
+        element.append(this.#element);
+
+        setTimeout(() => {
+            if (this.#element) {
+                this.remove();
+            }
+        }, this.#duration);
+
+        NotificationMessage.#activeNotification = this;
+    }
+
+    destroy() {
+        this.remove();
+        if (NotificationMessage.#activeNotification === this) {
+            NotificationMessage.#activeNotification = null;
+        }
+    }
+
+    remove() {
+        this.#element.remove();
+    }
+
+    get element() {
+        return this.#element;
+    }
+
+    get duration() {
+        return this.#duration;
+    }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,122 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+  #headers;
+  #data;
+  #element;
 
+  constructor(headers = [], data = []) {
+    this.#headers = headers;
+    this.#data = data;
+
+    this.#createElemente();
+  }
+
+  #createElemente() {
+    const tmp = document.createElement('div');
+    tmp.innerHTML = this.#template();
+    this.#element = tmp.firstElementChild;
+  }
+
+  #template() {
+    return `
+    <div class="sortable-table">
+      <div data-element="header" class="sortable-table__header sortable-table__row">
+        ${this.#headersTemplate()}
+      </div>
+
+      <div data-element="body" class="sortable-table__body">
+        ${this.#rowsTemplate()}
+      </div>
+    </div>`;
+  }
+
+  #headersTemplate() {
+    return this.#headers
+      .map(({ id, title, sortable }) => `
+        <div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}">
+          <span>${title}</span>
+        </div>
+      `)
+      .join('\n');
+  }
+
+  #rowsTemplate() {
+    let headersId = this.#headersId();
+
+    return this.#data
+      .map((row) => `
+        <div class="sortable-table__row">
+          ${headersId
+          .map(headerId => '<div class="sortable-table__cell">' + row[headerId] + '</div>')
+          .join('\n')
+        }
+        </div>
+      `)
+      .join('\n');
+  }
+
+  #headersId() {
+    return this.#headers
+      .map(header => header.id)
+      .reduce((accumulator, current) => {
+        accumulator.push(current);
+        return accumulator;
+      }, []);
+  }
+
+  #sortRows(header, direction) {
+    let { title, sortable, sortType } = this.#headers.filter(e => e.id === header)[0];
+    if (sortable === false) {
+      alert(`Column with title '${title}' can't be sorted`);
+      return;
+    }
+
+    if (sortType == 'number') {
+      this.#data.sort((a, b) => direction * (a[header] - b[header]));
+    } else if (sortType == 'string') {
+      this.#data.sort((a, b) => direction * (a[header].localeCompare(b[header], ['ru', 'en'], { caseFirst: 'upper' })));
+    } else {
+      throw new SyntaxError(`Invalide sortType '${sortType}'. It should be 'number' or 'string'`);
+    }
+
+    let body = this.#elementBody();
+    body.innerHTML = this.#rowsTemplate();
+    body.style.display = 'none';
+    body.style.display = '';
+  }
+
+  #elementHeader() {
+    return this.#element.querySelector('[data-element="header"]');
+  }
+
+  #elementBody() {
+    return this.#element.querySelector('[data-element="body"]');
+  }
+
+  destroy() {
+    this.#element.remove();
+  }
+
+  sort(header, param = 'asc') {
+    let headersId = this.#headersId();
+    if (headersId.includes(header)) {
+      if (param === 'asc') {
+        this.#sortRows(header, 1);
+      } else if (param === 'desc') {
+        this.#sortRows(header, -1);
+      } else {
+        throw new SyntaxError(`Invalide param '${param}'. It should be 'asc' or 'desc'`);
+      }
+    }
+  }
+
+  get element() {
+    return this.#element;
+  }
+
+  get subElements() {
+    return {
+      header: this.#elementHeader(),
+      body: this.#elementBody()
+    };
   }
 }
-
